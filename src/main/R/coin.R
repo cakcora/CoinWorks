4222415f7e46d73451b2f0055b4df4db7861c297
cxa123230 <cuneyt.akcora@fulbrightmail.org>
2017-10-10 00:02:17 -0500

    m3

--- src/main/R/coin.R
+++ src/main/R/coin.R
@@ -8,12 +8,15 @@ rm(list=ls(all=TRUE))
  
 
  
-wdata <- read.csv("C:/Users/cxa123230/IdeaProjects/CoinWorks/src/main/resources/transactions.txt",sep="\t",header=T)
-wdata$axis<-paste(wdata$year,"/",wdata$day) 
-  
- 
-m13<- ggplot(wdata,aes(x=paste(year,day),y=split))+geom_line(size=2)+scale_x_continuous(name="day")+theme_bw()+theme(legend.position = c(0.4, 0.8),text = element_text(size=16))+scale_y_continuous(limits=c(0.05,0.27),name="Gini value")
-m13<-m13+geom_point(aes(y=merge), size=4) +geom_point(aes(y=transition), size=4)   
-m13
-ggsave(filename=paste(resultFolder,"figs/giniUseless.eps",sep=""),plot=m13,width=5,height=3,unit="in")
-ggsave(filename=paste(resultFolder,"figs/giniUseless.png",sep=""),plot=m13,width=5,height=3,unit="in")
+wdata <- read.csv("C:/Users/cxa123230/IdeaProjects/CoinWorks/src/main/resources/trWeekly.txt",sep="\t",header=T)
+wdata$myx<-paste(wdata$year,"/",wdata$week) 
+wdata$ax<-as.factor(wdata$ax)  
+dfm <- melt(as.data.frame(wdata), id.vars=c("myx", "year","week"))
+
+m13<-ggplot(data = dfm, aes(x = myx,y=value,group=variable,colour=variable)) + 
+  geom_line() +scale_x_discrete(name="week")+scale_y_continuous(name="Percentage")+theme_bw()+theme(axis.text.x = element_text(angle = 90, hjust = 1),legend.position = c(0.8, 0.6),text = element_text(size=16))
+    
+
+resultFolder<-"C:/Users/cxa123230/Dropbox/Publications/PostDoc work/Blockchain Survey/R codes and Figures/"
+ggsave(filename=paste(resultFolder,"transactiontypes.eps",sep=""),plot=m13,width=5,height=3,unit="in")
+ggsave(filename=paste(resultFolder,"transactiontypes.png",sep=""),plot=m13,width=5,height=3,unit="in")
