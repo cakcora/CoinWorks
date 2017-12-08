87827b9faa338317a5d8a2c73c84e4edd0eb6fac
crnabay@gmail.com <crnabay@gmail.com>
2017-12-08 14:48:23 -0600

    .

---
+++ src/main/python/price_prediction_starter.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+# price processing
+# construct deep/wide price prediction model
+# print accuracy, precision, precall based on SVM, LR, RF, NB
+# print covariance correlation matrix on price
+
