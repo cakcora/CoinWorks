c9ce8c648d2854deb1e9059d31fae029c530d990
cxa123230 <cuneyt.akcora@fulbrightmail.org>
2018-02-01 18:05:11 -0600

    filtered chains for TDA

--- src/main/R/deeplearning.R
+++ src/main/R/deeplearning.R
@@ -10,49 +10,40 @@ readFile<-function(fileN){
   results <- read.csv(paste("D://bitcoin/createddata/results/",fileN,sep=""),sep="\t",header=F)
   return(results)
 }
+
+confusion<-function(results){
+  aggB<-nrow(results)
+  tp<-nrow(results[results$realLR>0&results$predictedLR>0,])/aggB
+  tn<-nrow(results[results$realLR<=0&results$predictedLR<=0,])/aggB
+  fn<- nrow(results[results$realLR>0&results$predictedLR<=0,])/aggB
+  fp<-nrow(results[results$realLR<=0&results$predictedLR>0,])/aggB
+  return(c(tp,fp,tn,fn))
+}
  
 
-fileDL = "occslidingPrediction_priceless.csv"#,"WithoutChainletslidingPrediction.csv"
+fileDL = "occslidingPrediction.csv" 
 resDL <- readFile(fileDL)
-colnames(resDL) <- c("priced","aggregated","window","horizon","slideTrain","slideTest","predictedLR","realLR","year","day")
-dataDL<- ddply(resDL,.(slideTrain,slideTest,window,horizon), summarize,
-                  meanDev = mean(abs(realLR-predictedLR)))
-dataDL$method<-"DL"
-
-fileB = "betti_prediction_priceless.csv"
-resBettiPriceless <-readFile(fileB)
-colnames(resBettiPriceless) <- c("allowed", "window","horizon","slideTrain","slideTest","predictedLR","realLR","year","day") 
-dataBetti<- ddply(resBettiPriceless,.(slideTrain,slideTest,window,horizon), summarize,
-              meanDev = mean(abs(realLR-predictedLR)))
-dataBetti$method<-"bettiPriceless" 
-  
+colnames(resDL) <- c("priced","feature","window","horizon","slideTrain","slideTest","predictedLR","realLR","year","day")
+dat<- ddply(resDL,.(priced,feature,slideTrain,slideTest,window,horizon), summarize,
+                  meanDev = mean(abs(realLR-predictedLR)), days=length(realLR))
+dat$method<-"chainlets"
+z<-ddply(resDL, .(priced,feature,slideTrain,slideTest,window,horizon), function(x) confusion(x))
+colnames(z) <- c("priced","feature","slideTrain","slideTest","window","horizon","tp","fp","tn","fn")
+aggDL<-cbind(z,dat$method,dat$meanDev,dat$days)
  
-fileBP = "betti_prediction_with_price.csv"
-resBettiPriced <-readFile(fileBP)
-colnames(resBettiPriced) <- c("allowed", "window","horizon","slideTrain","slideTest","predictedLR","realLR","year","day") 
-dataBettiP<- ddply(resBettiPriced,.(slideTrain,slideTest,window,horizon), summarize,
-                  meanDev = mean(abs(realLR-predictedLR)))
-dataBettiP$method<-"bettiPriced" 
 
 
-aggPDL<-nrow(resDL[resDL$realLR>0&resDL$predictedLR>0,])
-aggNDL<-nrow(resDL[resDL$realLR<=0&resDL$predictedLR<=0,])
-aggN<-nrow(resDL)
+fileB = "betti_prediction.csv"
+resBetti <-readFile(fileB)
+colnames(resBetti) <- c("priced","feature", "window","horizon","slideTrain","slideTest","predictedLR","realLR","year","day") 
+dat<- ddply(resBetti,.(priced,feature,slideTrain,slideTest,window,horizon), summarize,
+              meanDev = mean(abs(realLR-predictedLR)), days=length(realLR))
+dat$method<-"betti" 
+z<-ddply(resBetti, .(priced,feature,slideTrain,slideTest,window,horizon), function(x) confusion(x))
 
-aggPBettiPriceless<-nrow(resBettiPriceless[resBettiPriceless$realLR>0&resBettiPriceless$predictedLR>0,])
-aggNBettiPriceless<-nrow(resBettiPriceless[resBettiPriceless$realLR<=0&resBettiPriceless$predictedLR<=0,])
-aggB<-nrow(resBettiPriceless)
-
-aggPBPriced<-nrow(resBettiPriced[resBettiPriced$realLR>0&resBettiPriced$predictedLR>0,])
-aggNBPriced<-nrow(resBettiPriced[resBettiPriced$realLR<=0&resBettiPriced$predictedLR<=0,])
-aggBP<-nrow(resBettiPriced)
-aggResults<-(c(aggPDL/aggN,aggNDL/aggN,aggN,"DL"))
-aggResults<-rbind(aggResults,c(aggPBettiPriceless/aggB,aggNBettiPriceless/aggB,aggB,"betti priceless"),c(aggPBPriced/aggBP,aggNBPriced/aggBP,aggBP,"Betti priced"))
-colnames(aggResults)<-c("Positive","Negative","Total","Method")
-View(rbind(dataDL,dataBetti,dataBettiP) )
-View(aggResults)
-par(mfrow=c(1,4))
-plot(resBettiPriceless$predictedLR,resBettiPriceless$realLR)
-plot(resBettiPriced$predictedLR,resBettiPriced$realLR)
-plot(resDL$predictedLR,resDL$realLR)
-
+colnames(z) <- c("priced","feature","slideTrain","slideTest","window","horizon","tp","fp","tn","fn")
+aggBetti<-cbind(z,dat$method,dat$meanDev,dat$days)
+ 
+ 
+View( rbind(aggDL,aggBetti) )
+ 
